2a7fdc2932fdcea7618ec40699e72b219b069382
Adam Chlebek <CHLEBEAD@mail.gvsu.edu>
2019-04-22 01:42:48 -0400

    New UI Changes and starting on Algorithm

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,149 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnImageOne = new System.Windows.Forms.Button();
             this.pbImageOne = new System.Windows.Forms.PictureBox();
             this.pbImageTwo = new System.Windows.Forms.PictureBox();
-            this.btnImageTwo = new System.Windows.Forms.Button();
-            this.btnCompare = new System.Windows.Forms.Button();
-            this.lblStatus = new System.Windows.Forms.Label();
-            this.pbProgress = new System.Windows.Forms.ProgressBar();
-            this.gbStatistics = new System.Windows.Forms.GroupBox();
-            this.lblPictureOne = new System.Windows.Forms.Label();
-            this.lblPictureTwo = new System.Windows.Forms.Label();
+            this.btnUploadOne = new MaterialSkin.Controls.MaterialFlatButton();
+            this.btnUploadTwo = new MaterialSkin.Controls.MaterialFlatButton();
+            this.btnCompare = new MaterialSkin.Controls.MaterialFlatButton();
+            this.lblCurrent = new MaterialSkin.Controls.MaterialLabel();
+            this.lblPictureOne = new MaterialSkin.Controls.MaterialLabel();
+            this.lblPictureTwo = new MaterialSkin.Controls.MaterialLabel();
             ((System.ComponentModel.ISupportInitialize)(this.pbImageOne)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbImageTwo)).BeginInit();
-            this.gbStatistics.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // btnImageOne
-            // 
-            this.btnImageOne.Location = new System.Drawing.Point(13, 13);
-            this.btnImageOne.Name = "btnImageOne";
-            this.btnImageOne.Size = new System.Drawing.Size(145, 145);
-            this.btnImageOne.TabIndex = 0;
-            this.btnImageOne.Text = "Upload Image One";
-            this.btnImageOne.UseVisualStyleBackColor = true;
-            this.btnImageOne.Click += new System.EventHandler(this.BtnImageOne_Click);
             // 
             // pbImageOne
             // 
-            this.pbImageOne.Location = new System.Drawing.Point(164, 13);
+            this.pbImageOne.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImageOne.Location = new System.Drawing.Point(13, 119);
             this.pbImageOne.Name = "pbImageOne";
-            this.pbImageOne.Size = new System.Drawing.Size(145, 145);
-            this.pbImageOne.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbImageOne.Size = new System.Drawing.Size(250, 250);
+            this.pbImageOne.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbImageOne.TabIndex = 2;
             this.pbImageOne.TabStop = false;
             // 
             // pbImageTwo
             // 
-            this.pbImageTwo.Location = new System.Drawing.Point(164, 164);
+            this.pbImageTwo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImageTwo.Location = new System.Drawing.Point(410, 119);
             this.pbImageTwo.Name = "pbImageTwo";
-            this.pbImageTwo.Size = new System.Drawing.Size(145, 145);
-            this.pbImageTwo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbImageTwo.Size = new System.Drawing.Size(250, 250);
+            this.pbImageTwo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbImageTwo.TabIndex = 3;
             this.pbImageTwo.TabStop = false;
             // 
-            // btnImageTwo
+            // btnUploadOne
             // 
-            this.btnImageTwo.Location = new System.Drawing.Point(13, 164);
-            this.btnImageTwo.Name = "btnImageTwo";
-            this.btnImageTwo.Size = new System.Drawing.Size(145, 145);
-            this.btnImageTwo.TabIndex = 4;
-            this.btnImageTwo.Text = "Upload Image Two";
-            this.btnImageTwo.UseVisualStyleBackColor = true;
-            this.btnImageTwo.Click += new System.EventHandler(this.BtnImageTwo_Click);
+            this.btnUploadOne.AutoSize = true;
+            this.btnUploadOne.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnUploadOne.Depth = 0;
+            this.btnUploadOne.Icon = null;
+            this.btnUploadOne.Location = new System.Drawing.Point(52, 74);
+            this.btnUploadOne.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnUploadOne.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnUploadOne.Name = "btnUploadOne";
+            this.btnUploadOne.Primary = false;
+            this.btnUploadOne.Size = new System.Drawing.Size(159, 36);
+            this.btnUploadOne.TabIndex = 9;
+            this.btnUploadOne.Text = "Upload First Image";
+            this.btnUploadOne.UseVisualStyleBackColor = true;
+            this.btnUploadOne.Click += new System.EventHandler(this.BtnUploadOne_Click);
+            // 
+            // btnUploadTwo
+            // 
+            this.btnUploadTwo.AutoSize = true;
+            this.btnUploadTwo.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnUploadTwo.Depth = 0;
+            this.btnUploadTwo.Icon = null;
+            this.btnUploadTwo.Location = new System.Drawing.Point(449, 74);
+            this.btnUploadTwo.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnUploadTwo.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnUploadTwo.Name = "btnUploadTwo";
+            this.btnUploadTwo.Primary = false;
+            this.btnUploadTwo.Size = new System.Drawing.Size(176, 36);
+            this.btnUploadTwo.TabIndex = 10;
+            this.btnUploadTwo.Text = "Upload Second Image";
+            this.btnUploadTwo.UseVisualStyleBackColor = true;
+            this.btnUploadTwo.Click += new System.EventHandler(this.BtnUploadTwo_Click);
             // 
             // btnCompare
             // 
-            this.btnCompare.Location = new System.Drawing.Point(13, 346);
+            this.btnCompare.AutoSize = true;
+            this.btnCompare.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnCompare.Depth = 0;
+            this.btnCompare.Icon = null;
+            this.btnCompare.Location = new System.Drawing.Point(292, 333);
+            this.btnCompare.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnCompare.MouseState = MaterialSkin.MouseState.HOVER;
             this.btnCompare.Name = "btnCompare";
-            this.btnCompare.Size = new System.Drawing.Size(296, 23);
-            this.btnCompare.TabIndex = 5;
+            this.btnCompare.Primary = false;
+            this.btnCompare.Size = new System.Drawing.Size(86, 36);
+            this.btnCompare.TabIndex = 11;
             this.btnCompare.Text = "Compare";
             this.btnCompare.UseVisualStyleBackColor = true;
-            this.btnCompare.Click += new System.EventHandler(this.BtnCompare_Click);
+            this.btnCompare.Click += new System.EventHandler(this.BtnCompare_Click_1);
             // 
-            // lblStatus
+            // lblCurrent
             // 
-            this.lblStatus.AutoSize = true;
-            this.lblStatus.Location = new System.Drawing.Point(12, 386);
-            this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(102, 13);
-            this.lblStatus.TabIndex = 6;
-            this.lblStatus.Text = "Ready For Upload...";
-            // 
-            // pbProgress
-            // 
-            this.pbProgress.Location = new System.Drawing.Point(12, 402);
-            this.pbProgress.Name = "pbProgress";
-            this.pbProgress.Size = new System.Drawing.Size(297, 23);
-            this.pbProgress.TabIndex = 7;
-            // 
-            // gbStatistics
-            // 
-            this.gbStatistics.Controls.Add(this.lblPictureTwo);
-            this.gbStatistics.Controls.Add(this.lblPictureOne);
-            this.gbStatistics.Location = new System.Drawing.Point(12, 446);
-            this.gbStatistics.Name = "gbStatistics";
-            this.gbStatistics.Size = new System.Drawing.Size(297, 127);
-            this.gbStatistics.TabIndex = 8;
-            this.gbStatistics.TabStop = false;
-            this.gbStatistics.Text = "Statisitcs";
+            this.lblCurrent.AutoSize = true;
+            this.lblCurrent.Depth = 0;
+            this.lblCurrent.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lblCurrent.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblCurrent.Location = new System.Drawing.Point(12, 405);
+            this.lblCurrent.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lblCurrent.Name = "lblCurrent";
+            this.lblCurrent.Size = new System.Drawing.Size(138, 19);
+            this.lblCurrent.TabIndex = 12;
+            this.lblCurrent.Text = "Ready For Upload...";
             // 
             // lblPictureOne
             // 
             this.lblPictureOne.AutoSize = true;
-            this.lblPictureOne.Location = new System.Drawing.Point(7, 20);
+            this.lblPictureOne.Depth = 0;
+            this.lblPictureOne.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lblPictureOne.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblPictureOne.Location = new System.Drawing.Point(30, 451);
+            this.lblPictureOne.MouseState = MaterialSkin.MouseState.HOVER;
             this.lblPictureOne.Name = "lblPictureOne";
-            this.lblPictureOne.Size = new System.Drawing.Size(0, 13);
-            this.lblPictureOne.TabIndex = 0;
+            this.lblPictureOne.Size = new System.Drawing.Size(0, 19);
+            this.lblPictureOne.TabIndex = 14;
             // 
             // lblPictureTwo
             // 
             this.lblPictureTwo.AutoSize = true;
-            this.lblPictureTwo.Location = new System.Drawing.Point(6, 76);
+            this.lblPictureTwo.Depth = 0;
+            this.lblPictureTwo.Font = new System.Drawing.Font("Roboto", 11F);
+            this.lblPictureTwo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblPictureTwo.Location = new System.Drawing.Point(30, 518);
+            this.lblPictureTwo.MouseState = MaterialSkin.MouseState.HOVER;
             this.lblPictureTwo.Name = "lblPictureTwo";
-            this.lblPictureTwo.Size = new System.Drawing.Size(0, 13);
-            this.lblPictureTwo.TabIndex = 1;
+            this.lblPictureTwo.Size = new System.Drawing.Size(0, 19);
+            this.lblPictureTwo.TabIndex = 15;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(332, 585);
-            this.Controls.Add(this.gbStatistics);
-            this.Controls.Add(this.pbProgress);
-            this.Controls.Add(this.lblStatus);
+            this.ClientSize = new System.Drawing.Size(682, 659);
+            this.Controls.Add(this.lblPictureTwo);
+            this.Controls.Add(this.lblPictureOne);
+            this.Controls.Add(this.lblCurrent);
             this.Controls.Add(this.btnCompare);
-            this.Controls.Add(this.btnImageTwo);
+            this.Controls.Add(this.btnUploadTwo);
+            this.Controls.Add(this.btnUploadOne);
             this.Controls.Add(this.pbImageTwo);
             this.Controls.Add(this.pbImageOne);
-            this.Controls.Add(this.btnImageOne);
             this.Name = "Form1";
-            this.Text = "Compare Images";
+            this.Text = "Image Compare";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pbImageOne)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbImageTwo)).EndInit();
-            this.gbStatistics.ResumeLayout(false);
-            this.gbStatistics.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnImageOne;
         private System.Windows.Forms.PictureBox pbImageOne;
         private System.Windows.Forms.PictureBox pbImageTwo;
-        private System.Windows.Forms.Button btnImageTwo;
-        private System.Windows.Forms.Button btnCompare;
-        private System.Windows.Forms.Label lblStatus;
-        private System.Windows.Forms.ProgressBar pbProgress;
-        private System.Windows.Forms.GroupBox gbStatistics;
-        private System.Windows.Forms.Label lblPictureTwo;
-        private System.Windows.Forms.Label lblPictureOne;
+        private MaterialSkin.Controls.MaterialFlatButton btnUploadOne;
+        private MaterialSkin.Controls.MaterialFlatButton btnUploadTwo;
+        private MaterialSkin.Controls.MaterialFlatButton btnCompare;
+        private MaterialSkin.Controls.MaterialLabel lblCurrent;
+        private MaterialSkin.Controls.MaterialLabel lblPictureOne;
+        private MaterialSkin.Controls.MaterialLabel lblPictureTwo;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,53 +13,25 @@ using System.Windows.Forms;
 
 namespace Image_Comparison
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
         Image img1;
         Image img2;
+        int[] img1Return;
+        int[] img2Return;
 
         public Form1()
         {
             InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             Form1.CheckForIllegalCrossThreadCalls = false;
-        }
-
-        private void BtnImageOne_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog open = new OpenFileDialog();
-            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
-            if (open.ShowDialog() == DialogResult.OK)
-            {
-                pbImageOne.Image = new Bitmap(open.FileName); 
-                btnImageOne.Visible = false;
-                img1 = pbImageOne.Image;
-            }
-        }
-
-        private void BtnImageTwo_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog open = new OpenFileDialog();
-            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
-            if (open.ShowDialog() == DialogResult.OK)
-            {
-                pbImageTwo.Image = new Bitmap(open.FileName);
-                btnImageTwo.Visible = false;
-                img2 = pbImageTwo.Image;
-            }
-
-        }
-
-        private void BtnCompare_Click(object sender, EventArgs e)
-        {
-            var img1Thread = new Thread(() => readImage(img1, 1));
-            var img2Thread = new Thread(() => readImage(img2, 2));
-
-            img1Thread.Start();
-            img2Thread.Start();
         }
 
         public void readImage(Image img, int imageNum) {
@@ -66,13 +40,74 @@ namespace Image_Comparison
 
             if (imageNum == 1)
             {
-                lblPictureOne.Text = returns[0].ToString() + " " + returns[1].ToString() + " " + returns[2].ToString();
+                img1Return = returns;
             }
             else {
-                
-                lblPictureTwo.Text = returns[0].ToString() + " " + returns[1].ToString() + " " + returns[2].ToString();
+
+                img2Return = returns;
             }
 
+        }
+
+        private void BtnUploadOne_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                pbImageOne.Image = new Bitmap(open.FileName);
+                btnUploadOne.Visible = false;
+                img1 = pbImageOne.Image;
+            }
+        }
+
+        private void BtnUploadTwo_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                pbImageTwo.Image = new Bitmap(open.FileName);
+                btnUploadTwo.Visible = false;
+                img2 = pbImageTwo.Image;
+            }
+        }
+
+        public void waiting(Thread first, Thread second) {
+            while (true) {
+                if (!first.IsAlive && !second.IsAlive){
+                    break;
+                }
+            }
+
+            var doMathThread = new Thread(doMath);
+
+            doMathThread.Start();
+        }
+
+        private void doMath()
+        {
+            int redDifference = Math.Abs(img1Return[0] - img2Return[0]);
+            int greenDifference = Math.Abs(img1Return[1] - img2Return[1]);
+            int blueDifference = Math.Abs(img1Return[2] - img2Return[2]);
+
+            long total = redDifference + greenDifference + blueDifference;
+            lblCurrent.Text = (total).ToString();
+        }
+
+        private void BtnCompare_Click_1(object sender, EventArgs e)
+        {
+            if (img1 != null && img2 != null)
+            {
+                lblCurrent.Text = "Checking...";
+                var img1Thread = new Thread(() => readImage(img1, 1));
+                var img2Thread = new Thread(() => readImage(img2, 2));
+                var waitingThread = new Thread(() => waiting(img1Thread, img2Thread));
+
+                img1Thread.Start();
+                img2Thread.Start();
+                waitingThread.Start();
+            }
         }
     }
 }
